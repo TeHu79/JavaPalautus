6d90c9363a24e77777e294473647d5b91d3d4d92
Teija Hurme <teija.hurme@student.careeria.fi>
2025-02-07 20:19:15 +0200

    Perusversio valmis

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 //Express.js palvelin//
 const path = require('path')
 const express = require('express')
+const fs = require('fs').promises
 
 const app = express()
+
 
 const henkilokunta = require('./henkilokunta.json')
 
@@ -11,6 +13,19 @@ app.get('/api/henkilokunta', (req, res) => {
     res.json(henkilokunta)
     })
 
+// Pinkoodin lukeminen txt tiedostosta palvelimelta ja lähettäminen selaimelle
+app.get('/api/getpin', async (req, res) => {
+    try {
+        // Read the content of the text file
+        const savedPin = await fs.readFile('./pin.txt', 'utf-8')
+
+        // Send the file content of the response
+        res.send(savedPin)
+    } catch (error) {
+        console.error('Error reading file:', error)
+        res.status(500).send('Internal Server Error')
+    } 
+    })
 
 // Tehdään polkumääritys public kansioon 
 const polku = path.join(__dirname, './public')
